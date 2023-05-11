0128ac95162cb3b471a8735b332e8a5d6a2078c7
Amy Hoang Anh Johnson <72709428+amy-hoang-anh-johnson@users.noreply.github.com>
2023-05-11 16:35:17 +0100

    Create MyProfile.java

---
+++ MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Amy";
+    int age = 24;
+    double desiredSalary = 13.00;
+    char gender = 'F';
+    boolean lookingForJob = true;
+	}
+}
