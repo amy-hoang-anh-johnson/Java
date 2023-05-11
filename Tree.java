f214586f6c2128339988c994a59dc995735066c1
Amy Hoang Anh Johnson <72709428+amy-hoang-anh-johnson@users.noreply.github.com>
2023-05-11 15:29:59 +0100

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,14 @@
+// Define your tree class in this file!
+
+public class Tree {
+  public static void main(String[] args) {
+    //Planting a tree lol
+    System.out.println("Hey there, I’m Amy and I’m learning to code in Java!");
+    System.out.println("I’m going to plant a tree today!");
+    System.out.println("  *  ");
+    System.out.println("  **** ");
+    System.out.println(" ****  ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+  }
+}
